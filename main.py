71b915c9e95bd87e9bed2ca8bfb55b6bb0f54871
Jordan Hall <166601247+Pixi-J@users.noreply.github.com>
2024-04-10 18:05:50 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello world")
+# Github is confusing asf
+while True:
+    print("Menu\n"
+          "-------------\n"
+          "1. Encode\n"
+          "2. Decode\n"
+          "3. Quit\n")
+
+    men_opt = int(input("Please enter an option: "))
+
+    if men_opt == 1:
+        password = input("Please enter your password to encode: ")
+        encoded = ''
+        for digit in list(password):
+            encoded_digit = int(digit) + 3
+            encoded += str(encoded_digit)
+        print("Your password has been encoded and stored!\n")
+
+    if men_opt == 2:
+        pass
+
+    if men_opt == 3:
+        break
